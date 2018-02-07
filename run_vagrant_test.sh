27fa63ea6dbb9fcb7a2d0c21e8709fc019e4cb2d
indrajit <eendroroy@gmail.com>
2018-02-08 01:35:59 +0600

    update vagrant test runner

--- run_vagrant_test.sh
+++ run_vagrant_test.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 ansible --version
-ansible-playbook -e 'host_key_checking=False' vagrant/vagrant.yml -i vagrant/inventory --syntax-check
-ansible-playbook -e 'host_key_checking=False' vagrant/vagrant.yml -i vagrant/inventory
+ansible-playbook vagrant/vagrant.yml -i vagrant/inventory --syntax-check
+ansible-playbook vagrant/vagrant.yml -i vagrant/inventory
+
